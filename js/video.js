5eaa39143e7eec71cdf69ae032c7502b9330c604
natanderson19 <145033585+natanderson19@users.noreply.github.com>
2025-04-01 15:03:32 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -10,11 +10,11 @@ window.addEventListener("load", function() {
 	console.log('loop is set to ' + video.loop);
 });
 
-//play button
+//pay button
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	document.querySelector("#volume").textContent = Math.round(video.volume * 100) + "%";
+	documentg
  });
 //pause button
  document.querySelector("#pause").addEventListener("click", function() {
@@ -30,4 +30,73 @@ let speedFactor = 0.1
 	console.log("playback speed is " + video.playbackRate);
 });
 //speed up
-docum
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate += speedFactor;
+	console.log("playback speed is " + video.playbackRate);
+});
+
+//skip ahead 
+document.querySelector("#skip").addEventListener("click", function(){
+	let currentTime = video.currentTime;
+	let duration = video.duration;
+	if (currentTime + 10 > duration) {
+		video.currentTime = 0
+	}
+	else {
+		video.currentTime = currentTime + 10
+	}
+	console.log("current video time: " + video.currentTime + "seconds")
+})
+
+//old school/original
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+	console.log("old school applied");
+})
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+	console.log("original applied");
+})
+
+
+
+//volume and mute updates
+document.addEventListener("DOMContentLoaded", function() {
+    let video = document.getElementById("player1");
+    let volumeSlider = document.getElementById("slider");
+    let volumeDisplay = document.getElementById("volume");
+
+    //ensure video volume updates and is audible
+    function updateVolume() {
+        let volumeLevel = volumeSlider.value / 100;
+        video.volume = volumeLevel;
+        volumeDisplay.textContent = Math.round(volumeLevel * 100) + "%";
+    }
+    updateVolume();
+    // update volume when slider is changed
+    volumeSlider.addEventListener("input", updateVolume);
+
+	
+	//mute button toggle
+	muteButton = document.querySelector("#mute")
+	muteButton.addEventListener("click", function (){
+		if (video.volume > 0) {
+			video.dataset.previousVolume = video.volume;
+			video.volume = 0;
+			volumeSlider.value = 0;
+			volumeDisplay.textContent = "0%";
+			muteButton.textContent = "Unmute"
+			console.log("video muted");
+		}
+		else {
+			let previousVolume = video.dataset.previousVolume || 1;
+			video.volume = previousVolume;
+			volumeSlider.value = previousVolume * 100;
+			volumeDisplay.textContent = Math.round(previousVolume * 100) + "%";
+            muteButton.textContent = "Mute";
+			delete video.dataset.previousVolume;
+            console.log("Video unmuted");
+		}
+	});
+});
